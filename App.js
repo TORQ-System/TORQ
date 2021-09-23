bf15aa26aabf40a37a2d44ab6c6ceecfd1eb9498
Noura Alsulayfih <Nouraaalsulayfih@gmail.com>
2021-09-23 14:40:26 +0300

    Update App.js
    added more details for the view requests and integration steps illustrated in the screen.

--- App.js
+++ App.js
@@ -3,24 +3,8 @@ import type { Node } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as geolib from 'geolib';
 
-// ----------- Start of Nearest SRCA Center Logic --------------------- 
-
-// here we should have the location of the logged-in SRCA center (longitude and latitude) but we should have it from the login screen but for the sake of the logic we'll hard code it here ( we're assuming that we're malqa's SRCA center )
-const center = { latitude: 24.813340812816, longitude: 46.608601997063275, };
-
-// from firebase (shahad) this array should store all the requests in firebase.
-var requests = []
-
-// stores the request assigned to the logged in center's portal.
-var myCentersRequest = [];
-
-// for loop should be here ( to loop through the all the requests)
-
-// here is the location of the  accident request ( should be retrieved from firebase and loop through the requests )
-const accidentLocation = { latitude: 24.844351675476698, longitude: 46.810270618115474 };
-
-// here goes the function that will return the nearest SRCA center:
-var nearest = geolib.findNearest(accidentLocation, [
+// 1- here are ALL SRCA centers in riyadh with thier information (name , longitude , latitude).
+const SRCACenters = [
   {
     // 1-maseef
     name: "maseef",
@@ -140,72 +124,48 @@ var nearest = geolib.findNearest(accidentLocation, [
     name: "twaiq",
     latitude: 24.585265317480754,
     longitude: 46.547105141719804,
-  },
-  {
-    // 21-munsiyah
-    name: "munsiyah",
-    latitude: 24.824786989529542,
-    longitude: 46.81655385515806,
-  },
-  {
-    // 22-namar
-    name: "namar",
-    latitude: 24.55211787741151,
-    longitude: 46.63131015472793,
-  },
-  {
-    // 23-nadwah
-    name: "nadwah",
-    latitude: 24.79054888623931,
-    longitude: 46.876853868227386,
-  },
-  {
-    // 24-dar bida
-    name: "darBida",
-    latitude: 24.567103148393645,
-    longitude: 46.7685939682224,
-  },
-  {
-    // 25-bayan
-    name: "bayan",
-    latitude: 24.88285664307162,
-    longitude: 46.85941051240605,
-  },
-  {
-    // 26-Narjis
-    name: "Narjis",
-    latitude: 24.839968700490484,
-    longitude: 46.67215439706382,
-  },
-  {
-    // 27-salalm
-    name: "salalm",
-    latitude: 24.71603727390603,
-    longitude: 46.811516483566905,
-  },
-  {
-    // 28-qaserAlhukum
-    name: "qaserAlhukum",
-    latitude: 24.62625068416492,
-    longitude: 46.70996480685846,
   }
-]);
+];
 
-// check if we're the nearest center to request[i]
-if (nearest == center) {
-  myCentersRequest.append(accidentLocation)
+// 2- this array should store all the requests in firebase.
+var requests = []
+
+// 3- here we should have the location of the logged-in SRCA center ( here is almalqa's location ).
+const center = { latitude: 24.813340812816, longitude: 46.608601997063275, name: "almalqa" };
+
+// 4- stores the request assigned to the logged in center's portal.
+var myCentersRequest = [];
+
+// 5- here is the location of the  accident request ( this line should be inside the loop and should be retreived form myCentersRequest[index] )
+
+const accidentLocation = { latitude: 24.812681852154594, longitude: 46.605789041860184 };
+
+// 6- for loop should be here ( to loop through the all the requests and apply the conditions on each of them )
+
+// for (let index = 0; index < requests.length; index++) {
+
+// 7-  here goes the function that will return the nearest SRCA center:
+var nearest = geolib.findNearest(accidentLocation, SRCACenters);
+// as we mentioned earlier the Accident location should be retireved form the requests[index] but to test our code we hard coded it.
+
+// 8- check if we're the nearest center to request[index].
+if ((center.latitude == nearest.latitude) && (center.longitude == nearest.longitude)) {
+  myCentersRequest.push(accidentLocation);
 }
+// } end of for loop.
 
-//end of for loop
 
-// ----------- End of Nearest SRCA Center Logic --------------------- 
 
 const App = (): Node => {
   return (
-    <View>
-      <Text> The request f</Text>
-      <Text style={styles.text}> latitide: {nearest.latitude}</Text>
-      <Text style={styles.text}> longitude: {nearest.longitude}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}> I'm {center.name} SRCA center</Text>
+      <Text style={styles.text}> I have {myCentersRequest.length} requests</Text>
+      <View style={styles.sub}>
+        <Text style={styles.text}>The nearest SRCA Center to the accident is: </Text>
+        <Text style={styles.text}> nearest SRCA center latitide: {nearest.latitude}</Text>
+        <Text style={styles.text}> nearest SRCA center  longitude: {nearest.longitude}</Text>
+      </View>
     </View>
   );
 };
@@ -213,8 +173,16 @@ const App = (): Node => {
 const styles = StyleSheet.create({
   text: {
     color: 'white',
-    margin: 60,
+    marginBottom: 20,
   },
+  container: {
+    margin: 60,
+    marginTop: 100,
+  },
+  sub: {
+    marginTop: 60,
+  }
+
 });
 
 export default App;
